769ca7442014240f99ee587310c7c30c7356a1d3
Caroline <fromfontana@gmail.com>
2018-06-19 21:56:09 +0000

    Done.

---
+++ until.rb
@@ -0,0 +1,9 @@
+def using_until
+  lev_force = 6
+  until lev_force==10
+  puts "Wingardium Leviosa"
+  lev_force+=1
+end
+end
+
+using_until
--- while.rb
+++ while.rb
@@ -1,11 +1,7 @@
+def using_while 
 l_force = 6
 while l_force < 10 
   puts "Wingardium Leviosa"
   l_force+=1
 end 
-
-lev_force = 6
-until lev_force==10
-  puts "Wingardium Leviosa"
-  lev_force+=1
 end
