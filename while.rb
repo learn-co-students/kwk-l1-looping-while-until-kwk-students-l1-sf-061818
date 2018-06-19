35c4c46290d538e71df940f6ff0f4e652bfb012b
jackielim17 <jxckielim17@gmail.com>
2018-06-19 22:04:53 +0000

    Done.

---
+++ while.rb
@@ -0,0 +1,19 @@
+def while_
+  force=6
+  while force<10
+  puts "Wingardium Leviosa"
+  force+=1
+end 
+end
+
+while_
+
+def until_
+  force=6
+  until force==10
+  puts "Wingardium Leviosa"
+  force+=10
+end 
+end
+
+until_
